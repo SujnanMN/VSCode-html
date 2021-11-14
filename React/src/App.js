61d1bdc113e8ba5069b9b923aeeb66970bec84bb
SujnanMN <sujnansms99@gmail.com>
2021-11-14 21:24:23 +0530

    emp mgmt

--- React/src/App.js
+++ React/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
@@ -26,7 +26,7 @@ function App() {
       <h1 className={styles.success}>success</h1>
       <h1 className='error'>error</h1>
 
-      <Inline />
+      <Inline/>
       <Stylesheet enabled={true} />
       <Table />
       <NameList />
@@ -36,6 +36,39 @@ function App() {
       <EventBind />
 
     </div>
+  );
+}
+
+export default App; */
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import ListEmployeeComponent from './StudentMgmt/ListEmployeeComponent';
+import HeaderComponent from './StudentMgmt/HeaderComponent';
+import FooterComponent from './StudentMgmt/FooterComponent';
+import CreateEmployeeComponent from './StudentMgmt/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './StudentMgmt/UpdateEmployeeComponent';
+import ViewEmployeeComponent from './StudentMgmt/ViewEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+        <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Routes> 
+                          <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+                          <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+                          <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
+                          <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
+                          {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+                    </Routes>
+                </div>
+              <FooterComponent />
+        </Router>
+    </div>
+    
   );
 }
 

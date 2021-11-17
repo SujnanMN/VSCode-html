d4e792bfb99e758357bd1a45de862db6594ca674
SujnanMN <sujnansms99@gmail.com>
2021-11-17 16:56:30 +0530

    use effect component

--- React/src/App.js
+++ React/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import ClassCounter1 from './components/hooks/useeffect/ClassCounter1';
+import ClassMouse from './components/hooks/useeffect/ClassMouse';
 import CounterHook1 from './components/hooks/useeffect/CounterHook1';
+import HookMouse from './components/hooks/useeffect/HookMouse';
+import MouseContainer from './components/hooks/useeffect/MouseContainer';
 
 
 
@@ -9,7 +12,7 @@ import CounterHook1 from './components/hooks/useeffect/CounterHook1';
 function App() {
   return (
     <div className="App">
-    <CounterHook1/>
+      <MouseContainer />
     </div>
   );
 }

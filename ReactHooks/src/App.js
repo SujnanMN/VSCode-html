a6944a7c07402200bdbdc5a8007318b02864388c
SujnanMN <sujnansms99@gmail.com>
2021-11-22 11:19:33 +0530

    using an object as a state

--- ReactHooks/src/App.js
+++ ReactHooks/src/App.js
@@ -1,20 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import ComponentA from './components/contexthook/ComponentA';
 import React from 'react';
+import CounterTwo from './components/reducerhook/CounterTwo';
 
 
-export const UserContext = React.createContext()     //exporting a context
-export const BatchContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'Sujnan'} >
-        <BatchContext.Provider value={'DXC'}>
-          <ComponentA />
-        </BatchContext.Provider>
-      </UserContext.Provider>
+    <CounterTwo/>
     </div>
   );
 }

673f5f593432d5442e5a44401f73a9b8d5dd7dc5
SujnanMN <sujnansms99@gmail.com>
2021-11-23 16:37:51 +0530

    lesson 5 end

--- Node.js/L3-4-5/server.js
+++ Node.js/L3-4-5/server.js
@@ -1,9 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+const _= require('lodash');
 
 const server = http.createServer((req, res) => {
   // console.log(req);
-  console.log(req.url);
+  // console.log(req.url, req.method);
+  //lodash
+  const num = _.random(0,20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log("Running greet");
+  });
+  greet();
+  greet();
 
   // set header content type
   res.setHeader('Content-Type', 'text/html');
@@ -33,7 +43,7 @@ const server = http.createServer((req, res) => {
       path += 'about.html';
       res.statusCode = 200;
       break;
-    case '/about-us':
+    case '/about-me':
       res.statusCode = 301;
       res.setHeader('Location', '/about');
       res.end();

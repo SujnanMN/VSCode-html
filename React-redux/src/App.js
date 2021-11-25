89529d199d766abe541e2f9fd1711e676f2566b9
SujnanMN <sujnansms99@gmail.com>
2021-11-25 13:06:35 +0530

    Redux complete

--- React-redux/src/App.js
+++ React-redux/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import ChangeColor from "./components/ChangeColor";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Profile />
+      <Login />
+      <ChangeColor />
     </div>
   );
 }
